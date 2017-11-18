f3f9c928c542f25b5b657ad9daf498e4976e527a
Ryan Poole <rmp135@gmail.com>
2017-11-18 10:32:37 +0000

    Modified error page.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,17 @@
+<style lang="scss" scoped>
+  .container {
+    text-align: center;
+  }
+</style>
+
 <template lang="pug">
-  section.container.is-fluid
-    .columns
-      .column.is-4.is-offset-4
-        img(src="../assets/img/baby.png" alt="Logo" class="logo")
-        h1.title {{ error.statusCode }}
-        h2.subtitle {{ error.message }}
-        nuxt-link.button(to="/") Homepage
+  section.section
+    .container
+      .columns.is-centered
+        .column.is-4
+          h1.title {{ error.statusCode }}
+          h2.subtitle {{ error.message }}
+          nuxt-link.button(to="/") Homepage
 </template>
 
 <script>
